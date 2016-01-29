62bd4dc18ebc817fb01a6a5973e15bf9733de6b0
sebastianalcazarcs <salcazar.sp@gmail.com>
2016-01-29 21:50:46 +0000

    first commit

--- clock.js
+++ clock.js
@@ -1,22 +1,23 @@
-function drawClock() {
-    drawFace( global ctx,global radius);
-    drawNumbers(global ctx,global radius);
+
+
+function getTime(){
+var date = new Date();
+var year = date.getFullYear();
+var month = date.getMonth();
+var day = date.getDate();
+var hour = date.getHours();
+var minute = date.getMinutes();
+var second = date.getSeconds();
+if(hour>12){
+	hour=hour-12;
+}
+var time = hour + ":" + minute + ":" + second;
+
+var clock = document.getElementById("div");
+
+clock.innerHTML = time
+requestAnimationFrame(getTime);
 }
 
-function drawNumbers(ctx, radius) {
-    var ang;
-    var num;
-    ctx.font = radius*0.15 + "px arial";
-    ctx.textBaseline="middle";
-    ctx.textAlign="center";
-    for(num= 1; num < 13; num++){
-        ang = num * Math.PI / 6;
-        ctx.rotate(ang);
-        ctx.translate(0, -radius*0.85);
-        ctx.rotate(-ang);
-        ctx.fillText(num.toString(), 0, 0);
-        ctx.rotate(ang);
-        ctx.translate(0, radius*0.85);
-        ctx.rotate(-ang);
-    }
-}
+getTime()
+
